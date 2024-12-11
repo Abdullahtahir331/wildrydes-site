3373fbc45c9a30dee94f147b0c35eb4d9b8b5aa1
Abdullahtahir331 <108197266+Abdullahtahir331@users.noreply.github.com>
2024-12-11 08:33:45 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_WYZaipyVy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1uu1gqti0f1ie1e2v2klofjl56', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
